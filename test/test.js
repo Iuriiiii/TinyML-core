972a6f4348fe85fdd20571844f5590d38e13207a
Iuriiiii <metralicnetwork@gmail.com>
2022-06-26 01:03:22 -0300

    a little test fix

--- test/test.js
+++ test/test.js
@@ -1,5 +1,4 @@
-"use strict";
-const { assert } = require('assert');
+const assert = require('assert');
 const { translate, compile, langEngine } = require('../index.js');
 
 describe('Return Object', () =>
